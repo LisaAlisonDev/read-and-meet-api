3fa1c6b6ef4a2542b783756d79576f33d3868249
LisaAlisonDev <alisonbarretpro@gmail.com>
2022-10-20 09:15:04 +0400

    Added user profile creation on user registration.

--- api/app/Http/Controllers/Auth/RegisterController.php
+++ api/app/Http/Controllers/Auth/RegisterController.php
@@ -3,25 +3,24 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\User\UserProfileController;
+use App\Http\Helpers\UserHelper;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 
 class RegisterController extends Controller
 {
     /**
      * Registration
      */
-    public function register(Request $request): \Illuminate\Http\JsonResponse
+    public function register(Request $request, UserHelper $userHelper, UserProfileController $userProfileController): \Illuminate\Http\JsonResponse
     {
-        // vérifie le format de l'email
         if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)){
             return response()->json(['message' => 'Veuillez choisir une adresse email valide.', 'token' => null], 200);
         }
 
-        $existingUser = User::where('email', '=', $request->email)->get();
-
-        if (count($existingUser) == 1){
+        if ($userHelper->checkDuplicateEmail($request->email)){
             return response()->json(['message' => 'Cette adresse email existe déjà.', 'token' => null], 200);
         }
 
@@ -30,6 +29,8 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        $userProfileController->store($user->id);
 
         $token = $user->createToken('LaravelAuthApp')->accessToken;
 
